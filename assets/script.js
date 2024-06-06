09bf61fa64a5af7c4b4fc47f96c548f855446629
picky37 <malinowski.noe@gmail.com>
2024-06-06 10:04:00 +0200

    Suppression console.log

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,78 @@
-console.log('connecté...')
-
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
 
 
 const arrow_left = document.querySelector('.arrow_left');
-console.log(arrow_left);
 
 const arrow_right = document.querySelector('.arrow_right');
-console.log(arrow_right);
 
+const dots = document.querySelector('.dots');
+
+const banner_image = document.querySelector('.banner-img');
+
+const tagline = document.querySelector('#banner p');
+
+let indexn = 0;
+function miseAjourSlide() {
+
+	banner_image.src = `./assets/images/slideshow/${slides[indexn].image}`;
+
+	tagline.innerHTML = slides[indexn].tagLine;
+
+	const allDots = document.querySelectorAll('.dot');
+
+	allDots.forEach((dot, index) => {
+		dot.classList.remove('dot_selected');
+		if (index === indexn) {
+			dot.classList.add('dot_selected');
+		}
+	});
+}
 
 arrow_left.addEventListener('click', function () {
-    console.log('Flèche gauche cliquée !');
-    }
+	indexn--;
+	if (indexn < 0) {
+		indexn = slides.length - 1;
+	}
+	miseAjourSlide();
+}
 );
 
 arrow_right.addEventListener('click', function () {
-    console.log('Flèche droite cliquée !');
-    }
+	indexn++;
+	if (indexn >= slides.length) {
+		indexn = 0;
+	}
+	miseAjourSlide();
+}
 );
 
 
-var nombreElements = slides.length;
-console.log(nombreElements);
+for (let i = 0; i < slides.length; i++) {
+	dots.innerHTML +=
+		'<span id="dot' +
+		i +
+		'" class="dot" title="Image ' +
+		(i + 1) +
+		'"></span>';
+}
+
+miseAjourSlide();
+
